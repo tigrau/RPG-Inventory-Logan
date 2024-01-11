e76811ba90fecb3ddfeefd8a62bdc8c7154ea38d
tigrau <unstoppabletigra@gmail.com>
2024-01-11 05:14:14 +0300

    Inventory Interact Signal
    сигнал из Slot. Прожат слот сигнал - прожат слот - вызывает функцию которая отвечает взаимодйстиве с инвентарем в ней уже передается сам инвентарь и индекс

--- scenes/inventory/inventory_interface.gd
+++ scenes/inventory/inventory_interface.gd
@@ -3,4 +3,10 @@ extends Control
 @onready var player_inventory: PanelContainer = $PlayerInventory
 
 func set_player_inventory_data(inventory_data: InventoryData) -> void:
+	inventory_data.inventory_interact.connect(on_inventory_interact)
 	player_inventory.set_inventory_data(inventory_data)
+
+
+func on_inventory_interact(inventory_data: InventoryData, 
+		index: int,button: int) -> void:
+	print("%s %s %s" % [inventory_data, index, button])
--- scripts/castom_resources/inventory_data.gd
+++ scripts/castom_resources/inventory_data.gd
@@ -1,7 +1,9 @@
 class_name InventoryData
 extends Resource
 
+signal inventory_interact(inventory_data: InventoryData, index: int,button: int)
+
 @export var slot_datas : Array[SlotData]
 
 func on_slot_clicked(index: int,button: int) -> void:
-	print("inventory interact")
+	inventory_interact.emit(self, index, button)
