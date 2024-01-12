e53f9dc3f6eb86eb58320c2877e988ad5aa576ad
tigrau <unstoppabletigra@gmail.com>
2024-01-12 23:18:37 +0300

    Pick Up Script
    Пока только подьем если есть слот свободный - без мерджа

--- scripts/castom_resources/inventory_data.gd
+++ scripts/castom_resources/inventory_data.gd
@@ -44,6 +44,16 @@ func drop_single_slot_data(grabbed_slot_data: SlotData, index: int) -> SlotData:
 	else:
 		return null
 
+func pick_up_slot_data(slot_data: SlotData) -> bool:
+	
+	for index in slot_datas.size():
+		if not slot_datas[index]:
+			slot_datas[index] = slot_data
+			inventory_updated.emit(self)
+			return true
+	
+	return false
+
 func on_slot_clicked(index: int,button: int) -> void:
 	inventory_interact.emit(self, index, button)
 
